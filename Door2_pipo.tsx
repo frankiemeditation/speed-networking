ea3e9ae8f523ac80480ba85a31cc59f54f0fd5e9
frankiemeditation <112688742+frankiemeditation@users.noreply.github.com>
2022-09-07 15:30:31 +0200

    Modification murs

---
+++ Door2_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Door2_pipo" tilewidth="32" tileheight="32" tilecount="72" columns="6">
+ <image source="src/assets/Door2_pipo.png" width="192" height="384"/>
+</tileset>
---
+++ src/assets/Door3_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Door3_pipo" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="Door3_pipo.png" width="384" height="384"/>
+</tileset>
